0397d0f36f42b73a8ced93ffbfaf9e1f6fca3cec
Shannon Klett <sklett@connected.io>
2022-10-26 13:57:57 -0400

    Delimeter parent class

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -1,37 +1,42 @@
-
 class StringCalculator
 
   def add(str)
     return 0 if str.empty?
     return str.to_i unless str.include?(',') || str.include?('\n')
 
-    ints = getNumbers(str).split(str)
+    ints = Delimiter.create(str).split()
 
     negatives = ints.filter { |i| i.to_i < 0 }
     raise StandardError, "negatives not allowed: #{negatives.join(',')}" if negatives.length > 0
 
     ints.filter { |i| i.to_i <= 1000 }.sum { |x| x.to_i }
   end
+end
 
-  def getNumbers(str)
+class Delimiter
+  attr_reader :str
+
+  def initialize(str)
+    @str = str
+  end
+
+  def split()
+    str.split(/[\\n,]/)
+  end
+
+  def self.create(str)
     if str.include?('//')
-      CustomDelimiter.new
+      CustomDelimiter.new(str)
     else
-      DefaultDelimier.new
+      Delimiter.new(str)
     end
   end
+end
 
-  class CustomDelimiter
-    def split(str)
-      head, tail = str.split('\n')
-      delimiter = head.sub('//', '')
-      tail.split(delimiter)
-    end
-  end
-
-  class DefaultDelimier
-    def split(str)
-      str.split(/[\\n,]/)
-    end
+class CustomDelimiter < Delimiter
+  def split()
+    head, tail = str.split('\n')
+    delimiter = head.sub('//', '')
+    tail.split(delimiter)
   end
 end
